52b5a2c2a57d81a5abd38ea357aeaa47c5408f51
WansonChoi <wschoi.bhlab@gmail.com>
2019-05-13 17:26:10 +0900

    (CookHLA.py) Backbones.

--- CookHLA.py
+++ CookHLA.py
@@ -12,9 +12,121 @@ std_WARNING_MAIN_PROCESS_NAME = "\n[%s::WARNING]: " % (os.path.basename(__file__
 
 HLA_names = ["A", "B", "C", "DPA1", "DPB1", "DQA1", "DQB1", "DRB1"]
 
+TOLERATED_DIFF = 0.15
 
 
-def CookHLA(_input, _out, _reference, _geneticMap, _average_erate, _java_memory='2G'):
+def CookHLA(_input, _out, _reference, _geneticMap, _average_erate, _java_memory='2g',
+            _p_src = "./src", _p_dependency = "./dependency", __save_intermediates=False):
+
+
+    p_src = _p_src
+    p_dependency = _p_dependency
+
+    _p_plink = os.path.join(p_dependency, "plink")
+    _p_beagle4 = os.path.join(p_dependency, "beagle4.jar")
+    _p_linkage2beagle = os.path.join(p_dependency, "linkage2beagle.jar")
+    _p_beagle2linkage = os.path.join(p_dependency, "beagle2linkage.jar")
+    _p_beagle2vcf = os.path.join(p_dependency, "beagle2vcf.jar")
+    _p_vcf2beagle = os.path.join(p_dependency, "vcf2beagle.jar")
+
+
+    # Intermediate path.
+    if not _out:
+        print(std_ERROR_MAIN_PROCESS_NAME + 'The argument "{0}" has not given. Please check it again.\n'.format("--out"))
+        sys.exit()
+    else:
+        _out = _out if not _out.endswith('/') else _out.rstrip('/')
+        if bool(os.path.dirname(_out)): os.makedirs(os.path.dirname(_out), exist_ok=True)
+
+
+    ###### < Dependency Checking > ######
+
+    ### External software
+
+    ### Source files
+
+
+
+    ###### < Bash Command Preparation > ######
+
+    """
+    1. plink
+    2. beagle4
+    3. linkage2beagle
+    4. beagle2linkage
+    5. beagle2vcf
+    6. vcf2beagle
+    7. excluding_target_snp_not_reference
+    8. complete_header.R
+    9. Doubling_vcf.R
+    10. DP_min_selection.R
+    """
+
+    JAVATMP = _out+'.javatmpdir'
+    os.makedirs(JAVATMP, exist_ok=True)
+
+
+    # Memory representation check.
+
+    p = re.compile(r'g|G$')
+
+    if p.search(_java_memory):
+        _java_memory = p.sub(repl="000m", string=_java_memory) # Gigabyte to Megabyte to use it in java.
+    else:
+        print(std_ERROR_MAIN_PROCESS_NAME + "Given memory for beagle4 is unappropriate.\n"
+                                            "Please check '--java-memory/-mem' argument again.")
+        sys.exit()
+
+
+    PLINK = ' '.join([_p_plink, "--noweb", "--silent", '--allow-no-sex'])
+    BEAGLE4 = ' '.join(["java", '-Djava.io.tmpdir={}'.format(JAVATMP), "-Xmx{}".format(_java_memory), "-jar", _p_beagle4])
+    LINKAGE2BEAGLE = ' '.join(["java", '-Djava.io.tmpdir={}'.format(JAVATMP), "-Xmx{}".format(_java_memory), "-jar", _p_linkage2beagle])
+    BEAGLE2LINKAGE = ' '.join(["java", '-Djava.io.tmpdir={}'.format(JAVATMP), "-Xmx{}".format(_java_memory), "-jar", _p_beagle2linkage])
+    BEAGLE2VCF = ' '.join(["java", '-Djava.io.tmpdir={}'.format(JAVATMP), "-Xmx{}".format(_java_memory), "-jar", _p_beagle2vcf])
+    VCF2BEAGLE = ' '.join(["java", '-Djava.io.tmpdir={}'.format(JAVATMP), "-Xmx{}".format(_java_memory), "-jar", _p_vcf2beagle])
+
+
+
+    ###### < Control Flags > ######
+
+    EXTRACT_MHC = 1
+    FLIP = 1
+    CONVERT_IN = 1
+    IMPUTE = 1
+    CONVERT_OUT = 1
+    CLEAN_UP = 1
+
+
+
+
+    idx_process = 1
+
+
+    if EXTRACT_MHC:
+
+        idx_process += 1
+
+    if FLIP:
+
+        idx_process += 1
+
+    if CONVERT_IN:
+
+        idx_process += 1
+
+    if IMPUTE:
+
+        idx_process += 1
+
+    if CONVERT_OUT:
+
+        idx_process += 1
+
+    if CLEAN_UP:
+
+        idx_process += 1
+
+
 
 
 
@@ -76,17 +188,4 @@ if __name__ == "__main__":
 
 
 
-
-    ## Memory allocation.
-
-    p = re.compile(r'g|G$')
-
-    if p.search(args.java_memory):
-        args.java_memory = p.sub(repl="000m", string=args.java_memory) # Gigabyte to Megabyte to use it in java.
-    else:
-        print(std_ERROR_MAIN_PROCESS_NAME + "Given memory for beagle is unappropriate.\n"
-                                            "Please check '--java-memory/-mem' argument again.")
-        sys.exit()
-
-
-    CookHLA()
+    CookHLA(args.input, args.out, args.reference, args.genetic_map, args.average_erate, _java_memory=args.java_memory)
