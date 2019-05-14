1d242b52b4ee1428dafb8f305906344523377db9
WansonChoi <wschoi.bhlab@gmail.com>
2019-05-14 14:46:46 +0900

    (redefineBPv1BH.py) Slightly modified to work with CookHLA.py.

--- src/redefineBPv1BH.py
+++ src/redefineBPv1BH.py
@@ -9,15 +9,25 @@
 
 import sys, os
 
-[markerfile, outfile] = sys.argv[1:3]
+def redefineBP(_markerfile, _outfile):
 
-occupied={}
-with open(markerfile) as fin, open(outfile, 'w') as fout:
-    for l in fin:
-        [rsid, bp, al1, al2]=l.split()
-        bp=int(bp)
-        while bp in occupied:
-            bp+=1
-        occupied[bp]="%s %d %s %s\n"%(rsid, bp, al1, al2)
-    for key in sorted(occupied):
-        fout.write(occupied[key])
+    occupied={}
+    with open(_markerfile) as fin, open(_outfile, 'w') as fout:
+        for l in fin:
+            [rsid, bp, al1, al2]=l.split()
+            bp=int(bp)
+            while bp in occupied:
+                bp+=1
+            occupied[bp]="%s %d %s %s\n"%(rsid, bp, al1, al2)
+        for key in sorted(occupied):
+            fout.write(occupied[key])
+
+    return _outfile
+
+
+if __name__ == '__main__':
+
+
+    [markerfile, outfile] = sys.argv[1:3]
+
+    redefineBP(markerfile, outfile)
