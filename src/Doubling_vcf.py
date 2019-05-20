1a717bc37fdf9bf73e494f96b5d7c8f65ef08781
WansonChoi <wschoi.bhlab@gmail.com>
2019-05-20 14:37:56 +0900

    (Doubling_vcf.py) Revised version of 'Doubling_vcf.R'.

---
+++ src/Doubling_vcf.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+import os, sys, re
+import pandas as pd
+
+
+def Doubling_vcf(_input_vcf, _output_vcf):
+
+
+    return 0
+
+
+
+if __name__ == '__main__':
+
+    [input_vcf, output_vcf] = sys.argv[1:]
+
+
+    Doubling_vcf(input_vcf, output_vcf)
