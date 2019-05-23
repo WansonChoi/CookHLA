aed4e36a301157ccd1fe32499c068f3daec69413
Wanson Choi <wschoi.bhlab@gmail.com>
2019-05-23 14:26:36 +0900

    (BGL2SortBGl.py) Finally refined to use dictionary and generator.

--- src/BGL2SortBGl.py
+++ src/BGL2SortBGl.py
@@ -44,74 +44,66 @@ def BGL2SortBGL(sort_markers, inbgl, outbgl):
 def BGL2SortBGL_WS(sort_markers, inbgl, outbgl):
 
 
-    selectMarkers = []
-    first_time = True
-    conut_num=1
 
     ### Obtaining marker labels from sorted *.markers file(`sort_markers`)
+
+    selectMarkers = []
     with open(sort_markers) as fin:
         selectMarkers = [p.match(string=l).group(1) for l in fin]
 
 
-    ### Sorting the order of `inbgl` file's markers based on that of sorted markers(`sort_markers` -> `selectMarkers`)
+    ### Proprocessing input beagle file(`inbgl`) using enough memory
+    IN_BGL_header = {}
+    IN_BGL_body = {}
+
+    with open(inbgl) as pf:
+        for l in pf:
+
+            m = p.match(string=l)
+
+            if m.group(1) != 'M':
+                IN_BGL_header[m.group(2)] = l
+            else:
+                IN_BGL_body[m.group(2)] = l
+
+
+    # Header check.
+    # count = 0
+    #
+    # for k, v in IN_BGL_header.items():
+    #     print("{} : {}".format(k, v))
+    #     count += 1
+    #
+    #     if count > 5: break;
+    #
+    # print("Memory : %0.2f(Mb)"%(sys.getsizeof(IN_BGL_header)/1024**2))
+
+    # Body check.
+    # count = 0
+    #
+    # for k, v in IN_BGL_body.items():
+    #     print("{} : {}".format(k, v))
+    #     count += 1
+    #
+    #     if count > 5: break;
+    #
+    # print("Memory : %0.2f(Mb)"%(sys.getsizeof(IN_BGL_body)/1024**2))
+
+
+    ### Sorting the order of `inbgl` file's markers(rows) based on that of sorted markers(`sort_markers` -> `selectMarkers`)
     with open(outbgl, 'w') as of:
 
-        for t in selectMarkers:
-            with open(inbgl) as pf:
-                for l in pf:
+        # Writing Header
+        # print("Writing Header")
+        of.writelines((v for v in IN_BGL_header.values()))
 
-                    # c = l.split()
-                    # header = c[:2]
-                    # data = c[2:]
-
-                    m = p.match(string=l)
-                    header = [m.group(1), m.group(2)]
-
-                    if header[0] != "M" and first_time:
-                        of.write(l)
-                        # print(header)
-                    if t == header[1]:
-                        of.write(l)
-                        # print(header)
-                        conut_num = conut_num+1
-                        break
-
-                first_time = False
-
+        # Writing Body
+        # print("Writing Body")
+        of.writelines((IN_BGL_body[item] for item in selectMarkers))
 
 
     return outbgl
 
-
-# def GEN_MakeOutBGL(selectMarkers, inbgl, first_time, count_num):
-#
-#     for t in selectMarkers:
-#         with open(inbgl) as pf:
-#             for l in pf:
-#
-#                 # c = l.split()
-#                 # header = c[:2]
-#                 # data = c[2:]
-#
-#                 m = p.match(string=l)
-#                 header = [m.group(1), m.group(2)]
-#
-#                 if header[0] != "M" and first_time:
-#                     # newdata = []
-#                     # for j in range(len(data)):
-#                     #     newdata.append(data[j])
-#                     of.write(l)
-#                     print(header)
-#                 if t == header[1]:
-#                     # newdata = []
-#                     # for j in range(len(data)):
-#                     #     newdata.append(data[j])
-#                     of.write(l)
-#                     print(header)
-#                     conut_num = conut_num + 1
-#                     break
-#
-#             first_time = False
 
 
 
@@ -121,22 +113,24 @@ if __name__ == '__main__':
     """
     [BGL2SortBGL.py]
     
+    The module to sort the rows of given Input Beagle file(*.bgl(.phsed)) based on the order of given Input Marker file
+    (*.markers)
     
     """
 
     [sort_markers, inbgl, outbgl] = sys.argv[1:4]
 
-    # ### <Test> ###
-    #
+    ### <Test> ###
+
     # # (2019. 05. 22.) First Testing (due to too much slow result)
     # sort_markers = '/Users/wansun/Git_Projects/CookHLA/tests/modules/BGL2SortBGl/input/Exon234_Panel_test.markers'
     # inbgl = '/Users/wansun/Git_Projects/CookHLA/tests/modules/BGL2SortBGl/input/T1DGC_REF.STEP2_exon234.bgl.phased'
-    # outbgl = '/Users/wansun/Git_Projects/CookHLA/tests/modules/BGL2SortBGl/Exon234_Panel_test.sorted.bgl.phased'
+    # outbgl = '/Users/wansun/Git_Projects/CookHLA/tests/modules/BGL2SortBGl/Exon234_Panel_test.sorted.generator2.bgl.phased'
 
-    start = time.time()
+    # start = time.time()
 
     BGL2SortBGL_WS(sort_markers, inbgl, outbgl)
 
-    end = time.time()
+    # end = time.time()
 
     # print("Time : %0.2f"%((end - start)/60))
