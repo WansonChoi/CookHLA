cd7ba81a1419ed85c68e9ec1b18c27e85fa3d791
Wanson Choi <wschoi.bhlab@gmail.com>
2019-05-28 17:24:16 +0900

    (measureAccuracy.py) Init.

---
+++ src/measureAccuracy.py
@@ -0,0 +1,64 @@
+#-*- coding: utf-8 -*-
+import sys, os
+
+answerfile=sys.argv[1]
+predictfile=sys.argv[2]
+genes = sys.argv[3:]
+
+if len(genes)==0 or (len(genes)==1 and genes[0] == "all"):
+    genes="A B C DRB1 DPA1 DPB1 DQA1 DQB1".split()
+
+def main():
+    for gene in genes:
+        correct2d=0
+        total2d=0
+        correct4d=0
+        total4d=0
+        for i in range(1,11):
+            answers2d={}
+            answers4d={}
+            with open(answerfile) as fin:
+                for l in fin:
+                    c=l.split()
+                    ID=c[0]+' '+c[1]
+                    if c[2] == gene and len(c) > 3:
+                        answers2d[ID]=c[3].split(',')
+                        answers4d[ID]=c[4].split(',')
+            with open(predictfile) as fin:
+                for l in fin:
+                    c=l.split()
+                    ID=c[0]+' '+c[1]
+                    if c[2] == gene:
+                        predict2d=c[3].split(',')
+                        predict4d=c[4].split(',')
+                        if ID in answers2d:
+                            (correct, total)=compare_and_score(predict2d, answers2d[ID])
+                            correct2d+=correct
+                            total2d+=total
+                        if ID in answers4d:
+                            (correct, total)=compare_and_score(predict4d, answers4d[ID])
+                            correct4d+=correct
+                            total4d+=total
+
+        sys.stdout.write("%s\t2D\t%.5f\n"%(gene, float(correct2d)/total2d))
+        sys.stdout.write("%s\t4D\t%.5f\n"%(gene, float(correct4d)/total4d))
+
+
+def compare_and_score(predict, answer):
+    ## return value is correct, total
+    if answer[0] == '':
+        answer[0]='answer1'
+    if answer[1] == '':
+        answer[1]='answer2'
+    if predict[0] == '':
+        predict[0]='predict1'
+    if predict[1] == '':
+        predict[1]='predict2'
+
+    correct=max((answer[0]==predict[0])+(answer[1]==predict[1]), \
+            (answer[0]==predict[1])+(answer[1]==predict[0]))
+    return(correct,2)
+
+if __name__ == "__main__":
+    main()
+
