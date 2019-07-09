0d329835c86fe8dea694c0687f74ef7a1716f2ec
Wanson Choi <wschoi.bhlab@gmail.com>
2019-07-09 16:43:53 +0900

    (HLA_MultipleRefs.py) Removing exon234 panel after each 2,3,4 panel is created.

--- src/HLA_MultipleRefs.py
+++ src/HLA_MultipleRefs.py
@@ -14,8 +14,8 @@ std_ERROR_MAIN_PROCESS_NAME = "\n[%s::ERROR]: " % (os.path.basename(__file__))
 std_WARNING_MAIN_PROCESS_NAME = "\n[%s::WARNING]: " % (os.path.basename(__file__))
 
 HLA_names = ["A", "B", "C", "DPA1", "DPB1", "DQA1", "DQB1", "DRB1"]
-__EXON__ = ['exon2']
-# __EXON__ = ['exon2', 'exon3', 'exon4']
+# __EXON__ = ['exon2']
+__EXON__ = ['exon2', 'exon3', 'exon4']
 
 
 # Patterns to use.
@@ -79,6 +79,11 @@ class HLA_MultipleRefs():
             pool.join()
 
             self.ExonN_Panel = {_exonN_: _OUT.get() for _exonN_, _OUT in dict_Pool.items()}
+
+
+        # Remove exon_234 panel (assuming Each 2,3,4 panel successfully created.)
+        self.removeOUTPUT(self.EXON234_Panel)
+
 
 
 
