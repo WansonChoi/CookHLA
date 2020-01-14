0d9031c5352ec94d1642eaba48a980764233c251
WansonChoi <wschoi.bhlab@gmail.com>
2020-01-14 17:47:09 +0900

    (Panel-BGL2BED.sh) PLINK path.

--- src/MakeGeneticMap/Panel-BGL2BED.sh
+++ src/MakeGeneticMap/Panel-BGL2BED.sh
@@ -8,10 +8,12 @@
 ## $1: BGL prefix (X of X.bgl.phased and X.markers)
 ## $2: OUTPUT prefix
 ## $3: beagle2linkage.jar file
+## $4: plink(v1.9) # added by Wanson Choi. (2020. 01. 14.)
 
 BGL=$1
 OUTPUT=$2
 BEAGLE2LINKAGE=$3
+PLINK=$4
 
 cat $BGL.bgl.phased | java -jar $BEAGLE2LINKAGE $OUTPUT.tmp
 cut -d ' ' -f-5 $OUTPUT.tmp.ped > $OUTPUT.ped.left
@@ -26,8 +28,8 @@ paste -d ' ' $OUTPUT.map.rsid $OUTPUT.map.allele1 > $OUTPUT.refallele
 
 rm $OUTPUT.tmp.* $OUTPUT.map.* $OUTPUT.ped.*
 
-./plink --noweb --allow-no-sex --ped $OUTPUT.ped --map $OUTPUT.map --make-bed --reference-allele $OUTPUT.refallele --out $OUTPUT
-./plink --noweb --allow-no-sex --bfile $OUTPUT --keep-allele-order --freq --out $OUTPUT.FRQ
+${PLINK} --noweb --allow-no-sex --ped $OUTPUT.ped --map $OUTPUT.map --make-bed --reference-allele $OUTPUT.refallele --out $OUTPUT
+${PLINK} --noweb --allow-no-sex --bfile $OUTPUT --keep-allele-order --freq --out $OUTPUT.FRQ
 
 rm $OUTPUT.map $OUTPUT.ped $OUTPUT.log $OUTPUT.refallele $OUTPUT.FRQ.log
 
